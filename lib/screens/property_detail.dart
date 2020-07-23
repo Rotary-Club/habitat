1363884101fb9673e77b3a9787d518161050baed
Gary <gary.ong@utopia.my>
2020-07-24 01:22:30 +0800

    💄 Added animation on property detail

--- lib/screens/property_detail.dart
+++ lib/screens/property_detail.dart
@@ -7,8 +7,47 @@ import 'package:habitat/widgets/app_picture.dart';
 import 'package:habitat/widgets/app_scaffold.dart';
 import 'package:habitat/widgets/app_text.dart';
 
-class PropertyDetail extends StatelessWidget {
+class PropertyDetail extends StatefulWidget {
   const PropertyDetail({Key key}) : super(key: key);
+
+  @override
+  _PropertyDetailState createState() => _PropertyDetailState();
+}
+
+class _PropertyDetailState extends State<PropertyDetail>
+    with TickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+  double opacity1 = 0.0;
+  double opacity2 = 0.0;
+  double opacity3 = 0.0;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0, 1.0, curve: Curves.fastOutSlowIn)));
+    setData();
+    super.initState();
+  }
+
+  Future<void> setData() async {
+    animationController.forward();
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    setState(() {
+      opacity1 = 1.0;
+    });
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    setState(() {
+      opacity2 = 1.0;
+    });
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    setState(() {
+      opacity3 = 1.0;
+    });
+  }
 
   Widget _buildIcon(IconData icon, String text) {
     return Column(
@@ -188,88 +227,81 @@ class PropertyDetail extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                    left: 26,
                     top: MediaQuery.of(context).size.height * 0.30,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: AppBorderRadius.small,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: AppText(
-                          'Apartment',
-                          color: AppColor.white,
+                    left: 26,
+                    child: ScaleTransition(
+                      alignment: Alignment.center,
+                      scale: CurvedAnimation(
+                          parent: animationController,
+                          curve: Curves.fastOutSlowIn),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: AppBorderRadius.small,
                         ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 26,
-                    top: MediaQuery.of(context).size.height * 0.30,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: AppBorderRadius.small,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: AppText(
-                          'Apartment',
-                          color: AppColor.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: AppText(
+                            'Apartment',
+                            color: AppColor.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AppText(
-                            'Seri Mutiara Apartment',
-                            fontSize: 18,
-                          ),
-                          AppHeightSizedBox.doubleExtraSmallBox,
-                          AppText(
-                            'Shah Alam, Selangor',
-                            fontSize: 14,
-                            color: AppColor.grey,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: AppColor.green,
-                              borderRadius: AppBorderRadius.small,
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: opacity1,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppText(
+                              'Seri Mutiara Apartment',
+                              fontSize: 18,
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: AppText(
-                                'RM1500 /M',
-                                color: AppColor.white,
+                            AppHeightSizedBox.doubleExtraSmallBox,
+                            AppText(
+                              'Shah Alam, Selangor',
+                              fontSize: 14,
+                              color: AppColor.grey,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: AppColor.green,
+                                borderRadius: AppBorderRadius.small,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: AppText(
+                                  'RM1500 /M',
+                                  color: AppColor.white,
+                                ),
                               ),
                             ),
-                          ),
-                          AppHeightSizedBox.smallBox,
-                          AppText(
-                            'For Rent',
-                            fontSize: 11,
-                          ),
-                        ],
-                      ),
-                    ],
+                            AppHeightSizedBox.smallBox,
+                            AppText(
+                              'For Rent',
+                              fontSize: 11,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -280,63 +312,81 @@ class PropertyDetail extends StatelessWidget {
                 ),
               ),
               AppHeightSizedBox.smallBox,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: AppText('Description'),
-              ),
-              AppHeightSizedBox.extraSmallBox,
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: AppText(
-                  'Sky Condominium is the tallest landmark in Bandar Puchong Jaya. Situated 200 metres above sea level at the Hilltop with unobstructed views of the Puchong and KL skyline.',
-                  letterSpacing: 1.5,
-                  maxlines: 15,
-                  fontSize: 13,
-                ),
-              ),
-              AppHeightSizedBox.smallBox,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: AppText('Address'),
-              ),
-              AppHeightSizedBox.extraSmallBox,
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: AppText(
-                  'Persiaran Puchong Jaya Selatan, Bandar Puchong Jaya, 47100 Puchong, Selangor.',
-                  letterSpacing: 1.5,
-                  maxlines: 15,
-                  fontSize: 13,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Divider(
-                  height: 3,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: AppText('Amenities'),
-              ),
-              AppHeightSizedBox.smallBox,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: opacity2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _buildIcon(Icons.wifi, 'Wifi'),
-                    _buildIcon(Icons.pool, 'Pool'),
-                    _buildIcon(Icons.local_parking, 'Parking'),
-                    _buildIcon(Icons.local_drink, 'Bar'),
-                    _buildIcon(Icons.tv, 'TV'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: AppText('Description'),
+                    ),
+                    AppHeightSizedBox.extraSmallBox,
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: AppText(
+                        'Sky Condominium is the tallest landmark in Bandar Puchong Jaya. Situated 200 metres above sea level at the Hilltop with unobstructed views of the Puchong and KL skyline.',
+                        letterSpacing: 1.5,
+                        maxlines: 15,
+                        fontSize: 13,
+                      ),
+                    ),
+                    AppHeightSizedBox.smallBox,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: AppText('Address'),
+                    ),
+                    AppHeightSizedBox.extraSmallBox,
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: AppText(
+                        'Persiaran Puchong Jaya Selatan, Bandar Puchong Jaya, 47100 Puchong, Selangor.',
+                        letterSpacing: 1.5,
+                        maxlines: 15,
+                        fontSize: 13,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Divider(
-                  height: 3,
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: opacity3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Divider(
+                        height: 3,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: AppText('Amenities'),
+                    ),
+                    AppHeightSizedBox.smallBox,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildIcon(Icons.wifi, 'Wifi'),
+                          _buildIcon(Icons.pool, 'Pool'),
+                          _buildIcon(Icons.local_parking, 'Parking'),
+                          _buildIcon(Icons.local_drink, 'Bar'),
+                          _buildIcon(Icons.tv, 'TV'),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Divider(
+                        height: 3,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               _buildTenantDetails(context),
