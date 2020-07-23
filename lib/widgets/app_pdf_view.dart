73781209f7530a21254e824dee8617d4ac31f9aa
Ki Hong <kihonq@gmail.com>
2020-07-24 02:49:56 +0800

    ✨ Add AppPdfView

---
+++ lib/widgets/app_pdf_view.dart
@@ -0,0 +1,88 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:habitat/themes/app_theme.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
+
+class AppPdfViewer extends StatefulWidget {
+  const AppPdfViewer(
+    this.file, {
+    Key key,
+  }) : super(key: key);
+
+  final File file;
+
+  @override
+  _AppPdfViewerState createState() => _AppPdfViewerState();
+}
+
+class _AppPdfViewerState extends State<AppPdfViewer> {
+  int _actualPageNumber = 1, _allPagesCount = 0;
+  PdfController _pdfController;
+
+  @override
+  void initState() {
+    _pdfController = PdfController(
+      document: PdfDocument.openData(widget.file.readAsBytesSync()),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pdfController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(primaryColor: AppColor.accent),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('PdfView example'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.navigate_before),
+                onPressed: () {
+                  _pdfController.previousPage(
+                    curve: Curves.ease,
+                    duration: Duration(milliseconds: 100),
+                  );
+                },
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  '$_actualPageNumber/$_allPagesCount',
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.navigate_next),
+                onPressed: () {
+                  _pdfController.nextPage(
+                    curve: Curves.ease,
+                    duration: Duration(milliseconds: 100),
+                  );
+                },
+              ),
+            ],
+          ),
+          body: PdfView(
+            documentLoader: Center(child: CircularProgressIndicator()),
+            pageLoader: Center(child: CircularProgressIndicator()),
+            controller: _pdfController,
+            onDocumentLoaded: (document) {
+              setState(() {
+                _allPagesCount = document.pagesCount;
+              });
+            },
+            onPageChanged: (page) {
+              setState(() {
+                _actualPageNumber = page;
+              });
+            },
+          ),
+        ),
+      );
+}
