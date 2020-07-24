9d03b87396163db1b72bc0803a3011447217345a
Ki Hong <kihonq@gmail.com>
2020-07-25 01:01:21 +0800

    💄 Add share button in AppPdfViewr

--- lib/widgets/app_pdf_view.dart
+++ lib/widgets/app_pdf_view.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:habitat/screens/home_bottom_navigation.dart';
 import 'package:habitat/themes/app_theme.dart';
+import 'package:habitat/widgets/app_action_button.dart';
+import 'package:habitat/widgets/app_action_dialog.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 
 import 'app_scaffold.dart';
@@ -67,20 +70,55 @@ class _AppPdfViewerState extends State<AppPdfViewer> {
             },
           ),
         ],
-        child: PdfView(
-          documentLoader: Center(child: CircularProgressIndicator()),
-          pageLoader: Center(child: CircularProgressIndicator()),
-          controller: _pdfController,
-          onDocumentLoaded: (document) {
-            setState(() {
-              _allPagesCount = document.pagesCount;
-            });
-          },
-          onPageChanged: (page) {
-            setState(() {
-              _actualPageNumber = page;
-            });
-          },
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            PdfView(
+              documentLoader: Center(child: CircularProgressIndicator()),
+              pageLoader: Center(child: CircularProgressIndicator()),
+              controller: _pdfController,
+              onDocumentLoaded: (document) {
+                setState(() {
+                  _allPagesCount = document.pagesCount;
+                });
+              },
+              onPageChanged: (page) {
+                setState(() {
+                  _actualPageNumber = page;
+                });
+              },
+            ),
+            Positioned(
+              bottom: 40,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: AppActionButton(
+                    function: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AppActionDialog(
+                          title: 'Confirm Email?',
+                          content:
+                              'Tenant: jonas.kahnwald@sicmundus.com \nLanlord: matsenang@habitat.com',
+                          actionText: 'Send',
+                          function: () {
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomeBottomNavigation(),
+                                ),
+                                (Route<dynamic> route) => false);
+                          },
+                        ),
+                      );
+                    },
+                    title: 'Share',
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       );
 }
